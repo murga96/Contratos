4d3e42975331d3b9653d35a40bfbd0f0eca490d8
Gustavo Martínez de Murga <gustavo.murga1996@gmail.com>
2022-01-06 11:49:59 -0500

    Creating the graphql statements for Ejecutivos

--- src/database/GraphQLStatements.js
+++ src/database/GraphQLStatements.js
@@ -1,7 +1,7 @@
 import { gql } from "@apollo/client";
 
 export const selectAllTipoContrato = gql`
-  query findAllTipoContrato{
+  query findAllTipoContrato {
     findAllTipoContrato {
       idTipoContrato
       tipoContrato
@@ -24,133 +24,173 @@ export const updateTipoContrato = gql`
 `;
 
 export const removeTipoContrato = gql`
-  mutation removeTipoContrato($id: Int!){
-  removeTipoContrato(id:$id) {
-    tipoContrato
+  mutation removeTipoContrato($id: Int!) {
+    removeTipoContrato(id: $id) {
+      tipoContrato
+    }
   }
-}
 `;
 
 export const removeSeveralTipoContrato = gql`
-  mutation removeSeveralTipoContrato($id: [Int!]!){
-  removeSeveralTipoContrato(id: $id){
-    tipoContrato
+  mutation removeSeveralTipoContrato($id: [Int!]!) {
+    removeSeveralTipoContrato(id: $id) {
+      tipoContrato
+    }
   }
-}
 `;
 
 //Tipo de Clausulas
 export const selectAllTiposDeClausulas = gql`
-query selectAllTiposDeClausulas{
-  findAllTiposDeClausulas {
-    idTipoClausula,
-    nombre,
-    orden,
-    basesG,
-    compras,
-    cMarco,
-    excepcional, 
+  query selectAllTiposDeClausulas {
+    findAllTiposDeClausulas {
+      idTipoClausula
+      nombre
+      orden
+      basesG
+      compras
+      cMarco
+      excepcional
+    }
   }
-}
 `;
 
 export const updateTiposDeClausulas = gql`
   mutation createTiposDeClausulas($tipoClausula: CreateTiposDeClausulaInput!) {
     createTiposDeClausulas(createTiposDeClausulaInput: $tipoClausula) {
-      idTipoClausula,
-      nombre,
-      orden,
-      basesG,
-      compras,
-      cMarco,
-      excepcional, 
+      idTipoClausula
+      nombre
+      orden
+      basesG
+      compras
+      cMarco
+      excepcional
     }
   }
 `;
 
 export const removeTiposDeClausulas = gql`
-  mutation removeTiposDeClausulas($id: Int!){
-    removeTiposDeClausulas(id:$id) {
-    nombre
+  mutation removeTiposDeClausulas($id: Int!) {
+    removeTiposDeClausulas(id: $id) {
+      nombre
+    }
   }
-}
 `;
 
 export const removeSeveralTiposDeClausulas = gql`
-  mutation removeSeveralTiposDeClausulas($id: [Int!]!){
-  removeSeveralTiposDeClausulas(id: $id){
-    nombre
+  mutation removeSeveralTiposDeClausulas($id: [Int!]!) {
+    removeSeveralTiposDeClausulas(id: $id) {
+      nombre
+    }
   }
-}
 `;
 
 //Tipos de Documentos
 export const selectAllTiposDocumentos = gql`
-query selectAllTiposDocumentos{
-  findAllTiposDocumento {
-    idTipoDoc,
-    nombreDoc,
+  query selectAllTiposDocumentos {
+    findAllTiposDocumento {
+      idTipoDoc
+      nombreDoc
+    }
   }
-}
 `;
 
 export const updateTiposDocumentos = gql`
   mutation createTiposDocumentos($tipoDocumentos: CreateTiposDocumentoInput!) {
     createTiposDocumento(createTiposDocumentoInput: $tipoDocumentos) {
-      idTipoDoc,
+      idTipoDoc
       nombreDoc
     }
   }
 `;
 
 export const removeTiposDocumentos = gql`
-  mutation removeTiposDocumentos($id: Int!){
-    removeTiposDocumento(id:$id) {
+  mutation removeTiposDocumentos($id: Int!) {
+    removeTiposDocumento(id: $id) {
       nombreDoc
+    }
   }
-}
 `;
 
 export const removeSeveralTiposDocumentos = gql`
-  mutation removeSeveralTiposDocumentos($id: [Int!]!){
-  removeSeveralTiposDocumento(id: $id){
-    nombreDoc
+  mutation removeSeveralTiposDocumentos($id: [Int!]!) {
+    removeSeveralTiposDocumento(id: $id) {
+      nombreDoc
+    }
   }
-}
 `;
 
 //Cargo
 export const selectAllCargo = gql`
-query selectAllCargo{
-  findAllCargos {
-    idCargo,
-    cargo,
+  query selectAllCargo {
+    findAllCargos {
+      idCargo
+      cargo
+    }
   }
-}
 `;
 
 export const updateCargo = gql`
   mutation createCargo($cargo: CreateCargoInput!) {
     createCargo(createCargoInput: $cargo) {
-      idCargo,
-      cargo,
+      idCargo
+      cargo
     }
   }
 `;
 
 export const removeCargo = gql`
-  mutation removeCargo($id: Int!){
-    removeCargo(id:$id) {
-      cargo,
+  mutation removeCargo($id: Int!) {
+    removeCargo(id: $id) {
+      cargo
+    }
   }
-}
 `;
 
 export const removeSeveralCargo = gql`
-  mutation removeSeveralCargo($id: [Int!]!){
-    removeSeveralCargo(id: $id){
-    cargo,
+  mutation removeSeveralCargo($id: [Int!]!) {
+    removeSeveralCargo(id: $id) {
+      cargo
+    }
   }
-}
 `;
 
+//Ejecutivos
+export const selectAllCargo = gql`
+  query selectAllEjecutivos {
+    findAllEjecutivos {
+      idEjecutivo
+      nombre
+      cargo {
+        cargo
+      }
+      grupo {
+        grupos
+      }
+      activo
+    }
+  }
+`;
+
+export const updateEjecutivo = gql`
+  mutation createEjecutivo($ejecutivo: CreateEjecutivoInput!) {
+    createEjecutivo(createEjecutivoInput: $ejecutivo) {
+      idEjecutivo
+    }
+  }
+`;
+
+export const removeEjecutivo = gql`
+  mutation removeEjecutivo($id: Int!) {
+    removeEjecutivo(id: $id) {
+      idEjecutivo
+    }
+  }
+`;
+
+export const removeSeveralEjecutivo = gql`
+  mutation removeSeveralEjecutivo($id: [Int!]!) {
+    removeSeveralEjecutivo(id: $id) {
+      idEjecutivo
+    }
+  }
+`;
